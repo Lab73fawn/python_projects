4d3f6c2a7daf8f487b2096a2a24f500137f36e76
Lab73fawn <Thompson_david@student.mahoningctc.com>
2023-09-27 09:06:21 -0400

    first commit

---
+++ python_fundamentals_exam.py
@@ -0,0 +1,18 @@
+intro = print("Hello world")
+first_name = input("What is your first name?: ")
+
+# variables
+person_age = 17
+temperature = 58.0
+too_hot = 80
+
+# prints
+print(f"Your name is: {first_name}")
+print (f"{first_name}'s age is {person_age}")
+temp = input("What is the temperature?: ")
+
+if temperature < too_hot:
+ print("Too Hot")
+else:
+  print("Too Cold")
+
